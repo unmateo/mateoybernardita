bea0813ddf0625c4d927312c8108a87dd10385f7
Mateo Harfuch <mharfuch@gmail.com>
2025-02-19 20:10:52 -0300

    try

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,15 @@ export default function Page() {
   return (
     <div style={{ 
       backgroundColor: "#fffef8", 
-      height: '100vh', 
+      height: '100vh',
       display: 'flex', 
       justifyContent: 'center', 
       alignItems: 'center', 
-      overflow: 'hidden'
+      margin: 'auto'
     }}>
       <img src="/background.gif" alt="background" style={{ 
-          maxWidth: '100%', 
-          maxHeight: '80%', 
+          maxWidth: '70%', 
+          maxHeight: '70%', 
           objectFit: 'cover', 
           margin: '0px'
         }} />
