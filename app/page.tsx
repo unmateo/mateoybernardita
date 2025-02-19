6f4e9ea43557cdd36affa3f1abfeb80297c5195b
Mateo Harfuch <mharfuch@gmail.com>
2025-02-19 19:06:17 -0300

    background

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Page() {
-    return <h1>Mateo & Bernardita</h1>
+  return (
+    <div style={{ backgroundColor: "#fffef8", height: '100vh', textAlign: 'center' }}>
+      <img src="/background.png" alt="background" style={{ height: '100%' }} />
+      {/* <h1>Mateo & Bernardita</h1> */}
+    </div>
+  )
   }
