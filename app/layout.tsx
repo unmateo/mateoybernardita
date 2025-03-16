f84b2ef586e51885787d80e8214d75563cd4db30
Mateo Harfuch <mharfuch@gmail.com>
2025-03-16 16:35:48 -0300

    remove website

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,6 @@ export default function RootLayout({
         <meta property="og:description" content="¡Nos casamos!" />
         <meta property="og:image" content="https://www.mateoybernardita.com.ar/whatsapp.jpg" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.mateoybernardita.com.ar" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
         <link href="https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
