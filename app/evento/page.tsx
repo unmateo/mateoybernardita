fb33eeb7ec4fa12965c44231ca2c3922a400d469
Mateo Harfuch <mharfuch@gmail.com>
2025-06-08 21:08:49 -0300

    wip

--- app/evento/page.tsx
+++ app/evento/page.tsx
@@ -1,6 +1,5 @@
 import styles from './page.module.css';
 import Link from 'next/link';
-import InteractiveButton from './components/InteractiveButton';
 
 export default function EventoPage() {
   return (
@@ -11,27 +10,43 @@ export default function EventoPage() {
       </header>
 
       <section className={styles.section}>
-        <div className={styles.locationCard}>
-          <h3 className={styles.sectionTitle}>Ceremonia</h3>
-          <p className={styles.infoText}>
-            20 de Septiembre<br />
-            11:00 hs<br />
-            Deriva<br />
-            Dardo Rocha 2290, Martinez
-          </p>
-          <a 
-            href="https://maps.app.goo.gl/AMEU1myUznB2GhvW7" 
-            target="_blank" 
-            rel="noopener noreferrer"
-            className={styles.button}
-          >
-            Ver ubicación
-          </a>
+        <div className={styles.grid}>
+          <div className={styles.locationCard}>
+            <h3 className={styles.sectionTitle}>Cuándo</h3>
+            <p className={styles.infoText}>
+              20 de Septiembre<br />
+              11:00 hs<br />
+            </p>
+            <a 
+              href="https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MjdxNWwyYXVtM2sxZWo2cmJuM2ltMG9hcXIgMWFhMzVmZmNkMDlkMzM2YmE1ZDEyOGQ1MTk0MGJjYzY2MjY1ZmNjNTI5ZWM1NzhlMGI5ZjQ5MTBjZGM0OWYwMEBn&tmsrc=1aa35ffcd09d336ba5d128d51940bcc66265fcc529ec578e0b9f4910cdc49f00%40group.calendar.google.com" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className={styles.buttonInverted}
+            >
+              Agendar evento
+            </a>
+          </div>
+
+          <div className={styles.locationCard}>
+            <h3 className={styles.sectionTitle}>Dónde</h3>
+            <p className={styles.infoText}>
+              Deriva<br />
+              Dardo Rocha 2290, Martinez
+            </p>
+            <a 
+              href="https://maps.app.goo.gl/AMEU1myUznB2GhvW7" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className={styles.buttonInverted}
+            >
+              Ver ubicación
+            </a>
+          </div>
         </div>
       </section>
 
       <section className={styles.section}>
-        <h3 className={styles.sectionTitle}>¡Queremos compartir este día contigo!</h3>
+        <h3 className={styles.sectionTitle}>¡Queremos compartir este día con vos!</h3>
         <p className={styles.infoText}>
           Confirmá asistencia y completá algunas preguntas en el siguiente formulario.
         </p>
